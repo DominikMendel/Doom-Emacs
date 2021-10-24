bd49c1c42a3d851de2b2b4deaec5f75dcadd457d
Dominik <dominikmendel95@gmail.com>
2021-10-24 18:40:12 -0400

    Added template logic + Doom temp fix

--- init.el
+++ init.el
@@ -14,6 +14,9 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+
+(defvar native-comp-deferred-compilation-deny-list nil)
+
 (doom! :input
        ;;chinese
        ;;japanese
